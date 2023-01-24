7a860a90f4817be59db767b3cbf5b2ab81bd0ad2
Muskan Chauhan <chauhanmuskan1407@gmail.com>
2023-01-25 00:36:06 +0530

    datatypes in go

---
+++ dataTypes.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var data int
+	//declaration
+	//var Variable_name datatype
+
+	data = 10 //initialization
+	fmt.Println(data)
+
+	data1 := 100
+	fmt.Println(data1)
+
+	//data:=1000 //redeclare the variable  ERROR
+	data = 1000 //reassigning value
+
+	//datatype
+	//1. Primitive:
+	//int float bool string complex byte
+	//2. Non-Primitive
+	//struct map chan interface array slice
+
+	//functional programming language
+
+	name := "rahul"
+	fmt.Println(name)
+}

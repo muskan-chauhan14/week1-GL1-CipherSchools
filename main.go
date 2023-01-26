bb3b3ca934d24e57d4f3d3db1622f2b6d9ad9102
muskan-chauhan14 <92838471+muskan-chauhan14@users.noreply.github.com>
2023-01-26 14:14:59 +0530

    Rotational Cipher Exercism Problem

---
+++ main.go
@@ -0,0 +1,82 @@
+/*
+Create an implementation of the rotational cipher, also sometimes called the Caesar cipher.
+
+The Caesar cipher is a simple shift cipher that relies on transposing all the letters in the alphabet using an integer key between 0 and 26. Using a key of 0 or 26 will always yield the same output due to modular arithmetic. The letter is shifted for as many values as the value of the key.
+
+The general notation for rotational ciphers is ROT + <key>. The most commonly used rotational cipher is ROT13.
+
+A ROT13 on the Latin alphabet would be as follows:
+
+Plain:  abcdefghijklmnopqrstuvwxyz
+Cipher: nopqrstuvwxyzabcdefghijklm
+It is stronger than the Atbash cipher because it has 27 possible keys, and 25 usable keys.
+
+Ciphertext is written out in the same formatting as the input including spaces and punctuation.
+
+Examples
+ROT5 omg gives trl
+ROT0 c gives c
+ROT26 Cool gives Cool
+ROT13 The quick brown fox jumps over the lazy dog. gives Gur dhvpx oebja sbk whzcf bire gur ynml qbt.
+ROT13 Gur dhvpx oebja sbk whzcf bire gur ynml qbt. gives The quick brown fox jumps over the lazy dog.
+
+*/
+
+package main
+
+import "fmt"
+
+var alphabet = map[int]string{
+	1:  "a",
+	2:  "b",
+	3:  "c",
+	4:  "d",
+	5:  "e",
+	6:  "f",
+	7:  "g",
+	8:  "h",
+	9:  "i",
+	10: "j",
+	11: "k",
+	12: "l",
+	13: "m",
+	14: "n",
+	15: "o",
+	16: "p",
+	17: "q",
+	18: "r",
+	19: "s",
+	20: "t",
+	21: "u",
+	22: "v",
+	23: "w",
+	24: "x",
+	25: "y",
+	26: "z",
+}
+
+func GetKey(letter string) (key int) {
+	for index, value := range alphabet {
+		if value == letter {
+			key = index
+			break
+		}
+	}
+	return
+}
+
+//shifting it five times for beginner
+func RotationCipher(original string) (encryptic string) {
+	for _, value := range original {
+		shiftedKey := GetKey(string(value)) + 5
+		if shiftedKey > 26 {
+			shiftedKey = shiftedKey % 26
+		}
+		encryptic += alphabet[shiftedKey]
+	}
+	return
+}
+
+func main() {
+	fmt.Println(RotationCipher("omg"))
+}
